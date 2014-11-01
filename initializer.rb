0607820530f7547180217d2083bb97b432b83858
Andrew Burleson <andrew.burleson@citrusbyte.com>
2014-11-01 19:01:16 -0400

    Adjust initializer for Heroku

--- initializer.rb
+++ initializer.rb
@@ -2,8 +2,8 @@ require 'pg'
 require 'active_record'
 require 'geocoder'
 
-DATABASE_URL = "postgres://Andrew@localhost/tests"
-ActiveRecord::Base.establish_connection(DATABASE_URL)
+ENV['DATABASE_URL'] ||= "postgres://Andrew@localhost/tests"
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 
 begin
   ActiveRecord::Base.connection.execute "CREATE TABLE neighborhoods (id serial PRIMARY KEY, name varchar(50) NOT NULL, poly polygon NOT NULL);"
